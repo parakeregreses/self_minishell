b05f638d51ec07ee4f00d45abfa4583ced78a43c
Parakeregreses <judith.lb.s2i@gmail.com>
2025-08-23 15:53:58 +0200

    exit

--- built_in/exit.c
+++ built_in/exit.c
@@ -6,13 +6,13 @@
 /*   By: jlaine-b <jlaine-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 17:52:56 by liulm             #+#    #+#             */
-/*   Updated: 2025/08/23 15:52:05 by jlaine-b         ###   ########.fr       */
+/*   Updated: 2025/08/23 15:53:14 by jlaine-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cmd_exit(char **args, char ***envp, int *status)
+void	cmd_exit(char **args, char ***envp, int *status, int ok)
 {
 	(void) args;
 

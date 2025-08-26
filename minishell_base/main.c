8ccc33ca477969b3893c7b596c81e7642e08ae22
Parakeregreses <judith.lb.s2i@gmail.com>
2025-08-26 18:50:32 +0200

    SHLVL

--- minishell_base/main.c
+++ minishell_base/main.c
@@ -13,6 +13,34 @@
 #define _GNU_SOURCE
 #include "minishell.h"
 
+
+char	*shlvlline(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(envp[i], "SHLVL=", 6) == 0)
+			return (envp[i]);
+		i++;
+	}
+	return (NULL);
+}
+char	**update_SHLVL(char ***envp, int *status)
+{
+	char	*str;
+	int		shell_level;
+
+	str = shlvlline(*envp);
+	if (str == NULL)
+		return (cmd_export(envp, "SHLVgit L=1", status, 1));
+	shell_level = atoi(str + 6);
+	free(str);
+	str = ft_strjoinfree(ft_strdup("SHLVL="), ft_itoa(shell_level + 1));
+	return (cmd_export(envp, str, status, 1));
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char	***our_envp;
@@ -26,6 +54,7 @@ int	main(int argc, char **argv, char **envp)
 		*our_envp = ft_copy_env(envp);
 		status = malloc(sizeof(int));
 		*status = 0;
+		*our_envp = update_SHLVL(our_envp, status);
 		launching(our_envp, status);
 	}
 	else
